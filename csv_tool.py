5e01968ee70f68cfbe66b2e56bf57e475c6f9e6a
LLThomas <zs033@qq.com>
2021-04-24 16:10:21 +0800

    fix some bugs

--- csv_tool.py
+++ csv_tool.py
@@ -2,20 +2,16 @@ import pandas as pd
 
 if __name__ == '__main__':
     # omit ' ' and replace ' ' using T
-    with open('/Users/thomas/Downloads/test.csv', "r+", encoding="utf-8") as csv_file:
+    with open('/home/thomas/Downloads/data.csv', "r+", encoding="utf-8") as csv_file:
         content = csv_file.read()
-    with open('/Users/thomas/Downloads/test.csv', "w+", encoding="utf-8") as csv_file:
+    with open('/home/thomas/Downloads/data.csv', "w+", encoding="utf-8") as csv_file:
         csv_file.write(content.replace(' ', 'T'))
-    with open('/Users/thomas/Downloads/test.csv', "r+", encoding="utf-8") as csv_file:
+    with open('/home/thomas/Downloads/data.csv', "r+", encoding="utf-8") as csv_file:
         content = csv_file.read()
-    with open('/Users/thomas/Downloads/test.csv', "w+", encoding="utf-8") as csv_file:
+    with open('/home/thomas/Downloads/data.csv', "w+", encoding="utf-8") as csv_file:
         csv_file.write(content.replace('"', ''))
 
     # replace ' ' using 'Z'
-    f = pd.read_csv('/Users/thomas/Downloads/test.csv', header=None)
-    i = 0
-    for row in f[1]:
-        row = row + 'Z'
-        f[1][i] = row
-        i = i + 1
-    f.to_csv('/Users/thomas/Downloads/test.csv', header=False, index=False)
+    f = pd.read_csv('/home/thomas/Downloads/data.csv', low_memory=False)
+    f['time'] = f.apply(lambda x: x['time']+'Z', axis=1)
+    f.to_csv('/home/thomas/Downloads/data.csv', index=False)
